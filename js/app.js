5a9451fefce8650691445eaaded7d0b203192a37
Duy Hoang Hua <huahoangduy.389@gmail.com>
2021-08-08 15:26:25 +0700

    add video section

--- js/app.js
+++ js/app.js
@@ -17,3 +17,37 @@ $('.slides-top-news').owlCarousel({
         }
     }
 });
+$('.slides-video-news').owlCarousel({
+    items: 2,
+    dots: false,
+    nav: true,
+    navText: navText,
+    margin: 15,
+    responsive: {
+        500: {
+            items: 2
+        },
+        1280: {
+            items: 4
+        },
+        1600: {
+            items: 5
+        }
+    }
+});
+$(document).ready(function () {
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollup').fadeIn();
+    } else {
+      $('.scrollup').fadeOut();
+    }
+  });
+
+  $('.scrollup').click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+
+});
